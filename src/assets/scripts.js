57475667c7499ed40a6e21a05779128ec891a0ab
fcosmeo <frankcosmesystem@gmail.com>
2017-10-12 10:27:31 -0500

    n

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -7,6 +7,11 @@ $(function() {
     format: 'dd/mm/yyyy',
     startView: 2,
     startDate: '01/01/1900',
-    endDate: f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear()
+    endDate: f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear(),
+    days: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+    daysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+    daysMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Set', 'Oct', 'Nov', 'Dic'],
   });
 });
